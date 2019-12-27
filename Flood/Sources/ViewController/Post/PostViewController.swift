53ba38105b87639a26fe0473d63cee5755b1ed07
오준현 <oh.junhyeon95@gmail.com>
2019-12-27 23:57:51 +0900

    feature: PostView collectionview, imagePicker

--- Flood/Sources/ViewController/Post/PostViewController.swift
+++ Flood/Sources/ViewController/Post/PostViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import FMPhotoPicker
+//import FMPhotoPicker
 //import Tagging
 
 class PostViewController: UIViewController {
@@ -19,10 +19,13 @@ class PostViewController: UIViewController {
     @IBOutlet weak var postTextView: UITextView!
     @IBOutlet weak var urlTextField: UITextField!
     @IBOutlet weak var imagePickerBtn: UIButton!
+    @IBOutlet weak var postImageCV: UICollectionView!
     
     // MARK: - Variables and Properties
     var keycnt:Bool = false
     var attachmentViewYPosition:CGFloat = 0
+    let picker = UIImagePickerController()
+    var pickedIMG : UIImage?
     
     // MARK: - Dummy Data
     
@@ -39,9 +42,9 @@ class PostViewController: UIViewController {
         
         postTextView.delegate = self
         urlTextField.delegate = self
-        
-        //        picker.delegate = self
-        
+        postImageCV.delegate = self
+        postImageCV.dataSource = self
+        picker.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -59,7 +62,7 @@ class PostViewController: UIViewController {
         self.postTextView.text = ""
         self.postTextView.placeholder = "내용을 입력해주세요"
         attachmentViewYPosition = self.attachmentView.frame.origin.y
-        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(endTextFieldEditing)))
+//        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(endTextFieldEditing)))
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -87,10 +90,13 @@ class PostViewController: UIViewController {
         attachmentView.layer.addBorder([.top, .bottom], color: .veryLightPinkTwo, width: 1)
     }
     
+    @objc func pickImage() {
+        self.present(self.picker, animated: true) // Controller이기 때문에 present 메서드를 이용해서 컨트롤러 뷰를 띄워준다!
+    }
+    
     func addKeyboardNotification() {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-        
     }
     
     @objc private func keyboardWillShow(_ notification: Notification)  {
@@ -98,9 +104,7 @@ class PostViewController: UIViewController {
             let keybaordRectangle = keyboardFrame.cgRectValue
             let keyboardHeight = keybaordRectangle.height
             let tabbarHeight = self.tabBarController?.tabBar.frame.size.height
-            
             if keycnt == false {
-
                 attachmentView.frame.origin.y -= keyboardHeight - tabbarHeight!
                 keycnt = true
             }
@@ -114,10 +118,6 @@ class PostViewController: UIViewController {
         }
     }
 }
-
-
-
-
 
 extension PostViewController: UITextViewDelegate {
     func textViewDidChange(_ textView: UITextView) {
@@ -134,35 +134,56 @@ extension PostViewController: UITextViewDelegate {
 
 extension PostViewController: UITextFieldDelegate {
     
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
-        textField.resignFirstResponder()
-        return true
-    }
+//    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+//        textField.resignFirstResponder()
+//        return true
+//    }
     
-    @objc func endTextFieldEditing(){
-        urlTextField.resignFirstResponder()
-        postTextView.resignFirstResponder()
-    }
+//    @objc func endTextFieldEditing(){
+//        urlTextField.resignFirstResponder()
+//        postTextView.resignFirstResponder()
+//    }
 }
 
-extension PostViewController : FMPhotoPickerViewControllerDelegate {
+extension PostViewController : UICollectionViewDelegate {
     
-    func fmPhotoPickerController(_ picker: FMPhotoPickerViewController, didFinishPickingPhotoWith photos: [UIImage]) {
-        self.dismiss(animated: true, completion: nil)
-        //        previewImageView.image = photo
+}
+
+extension PostViewController : UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PostImageCVC", for: indexPath) as! PostImageCVC
+//        cell.postIMG.image = postIMG[indexPath.row]
+        cell.postIMG.backgroundColor = .black
+        
+        return cell
     }
-    
     
 }
-
-extension PostViewController : FMImageEditorViewControllerDelegate {
     
-    func fmImageEditorViewController(_ editor: FMImageEditorViewController, didFinishEdittingPhotoWith photo: UIImage) {
-        self.dismiss(animated: true, completion: nil)
+    
+extension PostViewController : UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        var newImage: UIImage? = nil
+        
+        if let possibleImage = info["UIImagePickerControllerEditedImage"] as? UIImage { // 수정된 이미지가 있을 경우
+            newImage = possibleImage
+        } else if let possibleImage = info["UIImagePickerControllerOriginalImage"] as? UIImage { // 오리지널 이미지가 있을 경우
+            newImage = possibleImage
+        }
+        
+//        = newImage // 받아온 이미지를 이미지 뷰에 넣어준다.
+        
+        picker.dismiss(animated: true) // 그리고 picker를 닫아준다.
     }
-    
-    
+
+
 }
 
 

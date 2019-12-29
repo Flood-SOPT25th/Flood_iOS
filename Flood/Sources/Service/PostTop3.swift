64f534fd3c49496367891d5fc6dd58b327685291
유현지 <skykk99@naver.com>
2019-12-29 18:49:25 +0900

    Start API PostTop3

---
+++ Flood/Sources/Service/PostTop3.swift
@@ -0,0 +1,68 @@
+//
+//  PostTop3.swift
+//  Flood
+//
+//  Created by hjyoo on 2019/12/29.
+//  Copyright © 2019 Junhyeon. All rights reserved.
+//
+/*
+ {
+ "message": "top3 피드 조회 성공",
+ "data": {
+   "topArr": [
+           {
+               "postImages": [
+                   "https://flood-master.s3.ap-northeast-2.amazonaws.com/1577096580808.jpg"
+               ],
+               "see": 2,
+               "bookmark": 11,
+               "bookmarked": true,
+               "bookmark_list": ["ehdgns1766@gmail.com"],
+               "score": 8.299999999999999,
+               "postDate": "2019-12-23T10:22:52.915Z",
+               "_id": "5e034125e37c092b1b92e222",
+               "image": "https://imgnews.pstatic.net/image/079/2019/12/23/0003304759_001_20191223163702507.jpg",
+               "title": "\"국과수 감정 조작\" 檢, 이춘재 8차 재심 개시 의견 제출",
+               "description": "[CBS노컷뉴스 고무성 기자] 검찰이 이춘재 연쇄살인 8차 사건에 대해 재심을 개시함이 상당하다는 의견서를 법원에 제출했다. 수원지검 이 사건 전담조사팀은 23일 오후 브리핑을 통해 \"재심청구인 윤모(52) 씨에 대",
+               "writer": "ehdgns1766",
+               "profileImage":"https://news.naver.com/main/read.mode=LSD&mid=shm&sid1=102&oid=079&aid=0003304759",
+               "comments_count": 2,
+               "postContent": "요",
+               "url": "https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=102&oid=079&aid=0003304759",
+               "groupCode": "1234",
+               "_somethingElse": 1
+           }
+ }
+ */
+
+import Foundation
+
+struct PostTop3: Codable {
+
+    let message: String
+    let data: [topArr]?
+}
+
+struct topArr: Codable {
+    
+    let postImages : String
+    let see : Int
+    let bookmark : Int
+    let bookmarked : Bool
+    let bookmark_list : String
+    let score : Double
+    let postDate : String
+    let _id : String
+    let image : String
+    let title : String
+    let description : String
+    let writer : String
+    let profileImage : String
+    let comments_count : Int
+    let postContent : String
+    let url : String
+    let groupCode : String
+    let _somethingElse : Int
+    
+
+}
---
+++ Flood/Sources/Service/PostTop3Services.swift
@@ -0,0 +1,59 @@
+//
+//  PostTop3Services.swift
+//  Flood
+//
+//  Created by hjyoo on 2019/12/29.
+//  Copyright © 2019 Junhyeon. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class PostTop3Services {
+    
+    //다른 class에서 방해하는 것을 막아주는 구문
+    private init() {}
+    
+    static let shared = PostTop3Services()
+
+    
+    //(completion: @escaping (enum으로선언한것<[Data첫번째구조체이름.두번째구조체이름], Error(swift타입)>) -> Void)
+    func getPostTop3(completion: @escaping (NetworkResult<Any>) -> Void) {
+        
+        let URL = APIConstants.PostTop3
+        Alamofire.request(URL).responseJSON {
+            response in
+            
+            switch response.result {
+                
+            case .success:
+                
+                //데이터가 들어왔는지 아닌지: 데이터가 안 들어왔을 때(= nil 일때) else실행
+                guard let data = response.data else { return }
+                
+                do{
+                    let decoder = JSONDecoder()
+                    
+                    let object = try decoder.decode(MainArticle.self, from: data)
+                    
+                    if object.success == true {
+                        print("성공")
+                        completion(.success(object.data))
+                    }
+                    else {
+                        completion(.requestErr(fatalError("서버이상")))
+                    }
+
+                } catch(let err) {
+                    print(err.localizedDescription)
+                    completion(.pathErr)
+                }
+            
+            case .failure(let err):
+                print(err.localizedDescription)
+                completion(.networkFail)
+                break
+            }
+        }
+    }
+}
